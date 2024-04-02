4348881334802ce42a593d4beb9bee16ba1f4117
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-04-02 18:00:33 +0330

    complate category item

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:apple_shop/widgets/banner_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -10,14 +11,73 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(),
+      ),
       home: SafeArea(
         child: Scaffold(
-          body: BannerSlider(),
+          body: Column(
+            children: [
+              BannerSlider(),
+              SizedBox(
+                height: 90,
+                child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return CategoryHorizontalItem(index);
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget CategoryHorizontalItem(int index) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 3),
+            width: 60,
+            height: 60,
+            decoration: ShapeDecoration(
+              shadows: const [
+                BoxShadow(
+                    blurRadius: 20,
+                    color: Colors.red,
+                    spreadRadius: -7,
+                    offset: Offset(0, 10))
+              ],
+              color: Colors.red,
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: const Icon(
+              Icons.select_all,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          const Text(
+            "تست",
+            style: TextStyle(fontFamily: "SB"),
+          )
+        ],
       ),
     );
   }
