097eaaa767cd5c06d3990830bc04d999165ed678
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-04-18 20:46:53 +0330

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:apple_shop/bloc/authentication/auth_bloc.dart';
-import 'package:apple_shop/data/datasource/authentication_datasource.dart';
-import 'package:apple_shop/data/repository/authentication_repository.dart';
 import 'package:apple_shop/di/service_locator.dart';
 import 'package:apple_shop/screan/bootom_navigation.dart';
-import 'package:apple_shop/screan/login_screan.dart';
-import 'package:apple_shop/screan/order_screan.dart';
-import 'package:apple_shop/util/auth_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding();
