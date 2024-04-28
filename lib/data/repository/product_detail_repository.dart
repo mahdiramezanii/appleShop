fdcbd5e8359e545352e74cd383a74161bf899f7a
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-04-28 22:08:34 +0330

    fix bug

--- lib/data/repository/product_detail_repository.dart
+++ lib/data/repository/product_detail_repository.dart
@@ -1,20 +1,35 @@
 import 'package:apple_shop/data/datasource/product_detail_datasource.dart';
 import 'package:apple_shop/data/models/product_gallery_model.dart';
+import 'package:apple_shop/data/models/product_varibent.dart';
 import 'package:apple_shop/di/service_locator.dart';
 import 'package:apple_shop/util/api_exception.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class IProductDetailRespotory {
-  Future<Either<String, List<PruductGallery>>> getProductGalleryImage(String product_id);
+  Future<Either<String, List<PruductGallery>>> getProductGalleryImage(
+      String product_id);
+  Future<Either<String, List<ProductVaribent>>> getProductVaribentList();
 }
 
 class ProductDetailRepository extends IProductDetailRespotory {
   final IProductDetailDataSource _productData = locator.get();
 
   @override
-  Future<Either<String, List<PruductGallery>>> getProductGalleryImage(String product_id) async {
+  Future<Either<String, List<PruductGallery>>> getProductGalleryImage(
+      String product_id) async {
     try {
       var response = await _productData.getProductGalleryImage(product_id);
+
+      return Right(response);
+    } on ApiExceptiopn catch (ex) {
+      return Left(ex.messgae);
+    }
+  }
+
+  @override
+  Future<Either<String, List<ProductVaribent>>> getProductVaribentList() async {
+    try {
+      var response = await _productData.getProductVaribentListt();
 
       return Right(response);
     } on ApiExceptiopn catch (ex) {
