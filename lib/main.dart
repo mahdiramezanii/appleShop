7e3274868fce3aae5d2129ee41d92ec67f9c5076
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-05-12 01:42:01 +0330

    fix bug in login and register screan with BlocCunsumer

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,26 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-GlobalKey<NavigatorState> globalKey=GlobalKey<NavigatorState>();
+GlobalKey<NavigatorState> globalKey = GlobalKey<NavigatorState>();
 
 void main() async {
-  
   WidgetsFlutterBinding();
   await Hive.initFlutter();
   Hive.registerAdapter(BucketAdapter());
   await Hive.openBox<Bucket>("BucketBox");
   await initLocator();
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-  
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey:globalKey,
+      navigatorKey: globalKey,
       debugShowCheckedModeBanner: false,
-      home: (AuthManager.isLoggin())
-          ? BottomNavigatonScrean()
-          : BlocProvider(
-              create: (context) => AuthBloc(),
-              child: LoginScrean(),
-            ),
+      home: (AuthManager.isLoggin()) ? BottomNavigatonScrean() : LoginScrean(),
     );
   }
 }
